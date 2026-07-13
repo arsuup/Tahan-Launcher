313cda1c79f73ab9066c9bfa67e45157553f2d75
arsuup <90505488+arsuup@users.noreply.github.com>
2026-07-13 15:54:04 +0200

    enviedeprendreunbuspleinetête V1.0.5

--- src/assets/js/panels/login.js
+++ src/assets/js/panels/login.js
@@ -2,111 +2,127 @@
  * @author Luuxis
  * Luuxis License v1.0 (voir fichier LICENSE pour les détails en FR/EN)
  */
-const { AZauth, Mojang } = require('minecraft-java-core');
+const { Mojang } = require('minecraft-java-core');
 const { ipcRenderer } = require('electron');
 
 import { popup, database, changePanel, accountSelect, addAccount, config, setStatus } from '../utils.js';
 
 class Login {
     static id = "login";
+
+    constructor() {
+        this.popupLogin = new popup();
+    }
+
+    // Le premier qui fait une remarque par rapport à mon code douteux, bah il a raison
+
     async init(config) {
         this.config = config;
         this.db = new database();
 
+        this.loginHome = document.querySelector('.login-home');
+        this.loginOffline = document.querySelector('.login-offline');
+        this.emailInput = document.querySelector('.email-offline');
+
         this.getLoginHome()
-
-        document.querySelector('.cancel-home').addEventListener('click', () => {
-            document.querySelector('.cancel-home').style.display = 'none'
-            changePanel('settings')
-        })
     }
 
-    async getLoginHome() {
-        console.log('Initializing Microsoft login...');
-        let popupLogin = new popup();
-        let loginHome = document.querySelector('.login-home');
-        let microsoftBtn = document.querySelector('.connect-home');
-        let crackBtn = document.querySelector('.connect-crack')
-        loginHome.style.display = 'block';
-
-        crackBtn.addEventListener("click", () => {
-            this.getCrack();
-            loginHome.style.display = 'none';
-        })
-
-        microsoftBtn.addEventListener("click", () => {
-            popupLogin.openPopup({
-                title: 'Connexion',
-                content: 'Veuillez patienter...',
-                color: 'var(--color)'
-            });
-
-            ipcRenderer.invoke('Microsoft-window', this.config.client_id).then(async account_connect => {
-                if (account_connect == 'cancel' || !account_connect) {
-                    popupLogin.closePopup();
-                    return;
-                } else {
-                    await this.saveData(account_connect)
-                    popupLogin.closePopup();
-                }
-
-            }).catch(err => {
-                popupLogin.openPopup({
-                    title: 'Erreur',
-                    content: err,
-                    options: true
-                });
-            });
-        })
-    }
-
-    async getCrack() {
-        console.log('Initializing offline login...');
-        let popupLogin = new popup();
-        let loginOffline = document.querySelector('.login-offline');
-
-        let emailOffline = document.querySelector('.email-offline');
-        let connectOffline = document.querySelector('.connect-offline');
-        let cancelOffline = document.querySelector('.cancel-offline')
-        loginOffline.style.display = 'block';
-
-        connectOffline.addEventListener('click', async () => {
-            if (emailOffline.value.length < 3) {
-                popupLogin.openPopup({
-                    title: 'Erreur',
-                    content: 'Votre pseudo doit faire au moins 3 caractères.',
-                    options: true
-                });
-                return;
-            }
-
-            if (emailOffline.value.match(/ /g)) {
-                popupLogin.openPopup({
-                    title: 'Erreur',
-                    content: 'Votre pseudo ne doit pas contenir d\'espaces.',
-                    options: true
-                });
-                return;
-            }
-
-            let MojangConnect = await Mojang.login(emailOffline.value);
-
-            if (MojangConnect.error) {
-                popupLogin.openPopup({
-                    title: 'Erreur',
-                    content: MojangConnect.message,
-                    options: true
-                });
-                return;
-            }
-            await this.saveData(MojangConnect)
-            popupLogin.closePopup();
+    handleMicrosoftBTNClick = () => {
+        this.popupLogin.openPopup({
+            title: 'Connexion',
+            content: 'Veuillez patienter...',
+            color: 'var(--color)'
         });
 
-        cancelOffline.addEventListener('click', () => {
-            this.getLoginHome();
-            loginOffline.style.display = 'none';
-        })
+        ipcRenderer.invoke('Microsoft-window', this.config.client_id).then(async account_connect => {
+            if (account_connect == 'cancel' || !account_connect) {
+                popupLogin.closePopup();
+            } else {
+                await this.saveData(account_connect)
+                popupLogin.closePopup();
+            }
+
+        });
+    };
+
+    handleCancelHomeBtn = () => {
+        return changePanel('settings')
+    };
+
+    async getLoginHome() {
+        this.loginHome.style.display = 'block';
+        const microsoftBtn = document.querySelector('.connect-home');
+        const crackBtn = document.querySelector('.connect-crack');
+        const cancelHomeBtn = document.querySelector('.cancel-home');
+
+        crackBtn.removeEventListener('click', this.handleCrackClick);
+        crackBtn.addEventListener('click', this.handleCrackClick);
+
+        microsoftBtn.removeEventListener("click", this.handleMicrosoftBTNClick);
+        microsoftBtn.addEventListener("click", this.handleMicrosoftBTNClick);
+
+        cancelHomeBtn.removeEventListener("click", this.handleCancelHomeBtn);
+        cancelHomeBtn.addEventListener("click", this.handleCancelHomeBtn);
+    }
+
+    handleCrackClick = () => {
+        this.getCrack();
+        document.querySelector('.login-home').style.display = 'none';
+    };
+
+    handleConnectOfflineClick = async () => {
+        const username = this.emailInput.value;
+
+        if (username.length < 3) {
+            this.popupLogin.openPopup({
+                title: 'Erreur',
+                content: 'Votre pseudo doit faire au moins 3 caractères.',
+                options: true
+            });
+            return;
+        }
+
+        if (username.includes(' ')) {
+            this.popupLogin.openPopup({
+                title: 'Erreur',
+                content: 'Votre pseudo ne doit pas contenir d\'espaces.',
+                options: true
+            });
+            return;
+        }
+
+        let MojangConnect = await Mojang.login(username);
+
+        if (MojangConnect.error) {
+            this.popupLogin.openPopup({
+                title: 'Erreur',
+                content: MojangConnect.message,
+                options: true
+            });
+            return;
+        }
+        await this.saveData(MojangConnect);
+        this.getLoginHome();
+        this.loginOffline.style.display = 'none';
+        return changePanel('settings');
+        //this.popupLogin.closePopup();
+    };
+
+    handleCancelOfflineClick = () => {
+        this.getLoginHome();
+        this.loginOffline.style.display = 'none';
+    };
+
+    async getCrack() {
+        this.loginOffline.style.display = 'block';
+        const connectOffline = document.querySelector('.connect-offline');
+        const cancelOffline = document.querySelector('.cancel-offline')
+
+        connectOffline.removeEventListener('click', this.handleConnectOfflineClick);
+        connectOffline.addEventListener('click', this.handleConnectOfflineClick);
+
+        cancelOffline.removeEventListener('click', this.handleCancelOfflineClick);
+        cancelOffline.addEventListener('click', this.handleCancelOfflineClick);
     }
 
     async saveData(connectionData) {
